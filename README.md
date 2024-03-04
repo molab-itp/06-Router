be4f9f74f8ba6c9f66a2e2498df17f7f4108238c
John Henry Thompson <jht9629@nyu.edu>
2024-03-04 18:10:01 -0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 06-Router
+# [06-Router](https://github.com/molab-itp/06-Router)
+
+- Replaces NavigationView with Router PageModel: ObservableObject
+
+- Based on https://github.com/molab-itp/03-ImageUiDemo-2-urls
+
+Using NavigationView to create list of images references using URL
+
+- Page1: UIImage from URL
+
+- Page2: Item struct for more info about image
+
+- Page3: List for scrolling
+
+- Page4: NavigationView with ItemDetail and ItemRow
+
+- Page5: UIImage named from Resource folder
+    - @State
+    - ui elements: Toggle, Slider, Picker
---
+++ Router/Router/MainView.swift
@@ -0,0 +1,49 @@
+//
+//  MainView.swift
+//  Router
+//
+//  Created by jht2 on 3/4/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @EnvironmentObject var pageModel: PageModel;
+
+    var body: some View {
+        VStack {
+            switch pageModel.pageTag {
+            case .Page1:
+                Page1()
+            case .Page3:
+                Page3()
+            case .Page5:
+                Page5()
+            }
+            Spacer()
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            HStack {
+                Button(action: {
+                    pageModel.pageTag = .Page1
+                }) {
+                    Text("[Page1]")
+                }
+                Button(action: {
+                    pageModel.pageTag = .Page3
+                }) {
+                    Text("[Page3]")
+                }
+                Button(action: {
+                    pageModel.pageTag = .Page5
+                }) {
+                    Text("[Page5]")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+        .environmentObject(PageModel())
+}
