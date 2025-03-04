32673f79415e51c11198c926580755336a7dcf3d
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-04 08:42:23 -0500

    indent

--- Router/Router/MainView.swift
+++ Router/Router/MainView.swift
@@ -10,40 +10,40 @@ import SwiftUI
 struct MainView: View {
   @Environment(PageModel.self) var pageModel
 
-    var body: some View {
-        VStack {
-            switch pageModel.pageTag {
-            case .Page1:
-                Page1()
-            case .Page3:
-                Page3()
-            case .Page5:
-                Page5()
-            }
-            Spacer()
-            Text("Hello, World!")
-            HStack {
-                Button(action: {
-                    pageModel.pageTag = .Page1
-                }) {
-                    Text("[Page1]")
-                }
-                Button(action: {
-                    pageModel.pageTag = .Page3
-                }) {
-                    Text("[Page3]")
-                }
-                Button(action: {
-                    pageModel.pageTag = .Page5
-                }) {
-                    Text("[Page5]")
-                }
-            }
+  var body: some View {
+    VStack {
+      switch pageModel.pageTag {
+      case .Page1:
+        Page1()
+      case .Page3:
+        Page3()
+      case .Page5:
+        Page5()
+      }
+      Spacer()
+      Text("Hello, World!")
+      HStack {
+        Button(action: {
+          pageModel.pageTag = .Page1
+        }) {
+          Text("[Page1]")
         }
+        Button(action: {
+          pageModel.pageTag = .Page3
+        }) {
+          Text("[Page3]")
+        }
+        Button(action: {
+          pageModel.pageTag = .Page5
+        }) {
+          Text("[Page5]")
+        }
+      }
     }
+  }
 }
 
 #Preview {
-    MainView()
-        .environment(PageModel())
+  MainView()
+    .environment(PageModel())
 }
--- Router/Router/Page3.swift
+++ Router/Router/Page3.swift
@@ -4,24 +4,24 @@
 import SwiftUI
 
 struct Page3: View {
-    var body: some View {
-        List {
-            ForEach(imageItems) { item in
-                HStack {
-                    Image(uiImage: imageFor(string: item.urlStr))
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:100, height: 100)
-                    Text(item.label)
-                    Spacer()
-                }
-            }
+  var body: some View {
+    List {
+      ForEach(imageItems) { item in
+        HStack {
+          Image(uiImage: imageFor(string: item.urlStr))
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+          Text(item.label)
+          Spacer()
         }
+      }
     }
+  }
 }
 
 struct Page3_Previews: PreviewProvider {
-    static var previews: some View {
-        Page3()
-    }
+  static var previews: some View {
+    Page3()
+  }
 }
